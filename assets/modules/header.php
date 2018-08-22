f18cc178cf418e3fda2356a499d631cb7b8cecc0
samoasao <samoasao@gmail.com>
2018-08-22 09:54:18 -0700

    switch to bootstrap 3.3.5

--- assets/modules/header.php
+++ assets/modules/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $site_title; ?></title>
     <script src="/assets/vendor/jquery/jquery-3.3.1.slim.min.js"></script>
-    <script src="/assets/vendor/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+    <script src="/assets/vendor/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
     <script src="/assets/js/main.js"></script>
     <link rel="stylesheet" href="/assets/css/style.css" type="text/css" media="screen" />
     <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond|Poiret+One|Proza+Libre" rel="stylesheet">
